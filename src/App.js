392c9efbaa5e898b054592c7e2a6942a22a2441f
Rajivee <jayarajivee@gmail.com>
2023-08-29 11:29:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
+import Dashboard from './components/admin/Dashboard'
+import FoodManagement from './components/admin/FoodManagement'
+import OrderItem from './components/admin/OrderItem'
+import Cart from './components/users/Cart'
+import Food from './components/users/Food'
+import Login from './components/users/Login'
+import Signup from './components/users/SignUp'
+import Success from './components/users/Success'
+import React, { useState } from 'react';
+export const CartContext = React.createContext();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [cart,setCart] = useState([])
+  return<>
+      <BrowserRouter>
+      <CartContext.Provider value={{cart,setCart}}>
+          <Routes>
+            <Route path='/dashboard' element={<Dashboard/>}/>
+            <Route path='/food-management' element={<FoodManagement/>}/>
+            <Route path='/dashboard/:id' element={<OrderItem/>}/>
+            <Route path='/user-cart' element={<Cart/>}/>
+            <Route path='/user-menu' element={<Food/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/signup' element={<Signup/>}/>
+            <Route path='/order-success' element={<Success/>}/>
+            <Route path='*' element={<Navigate to='/login'/>}/>
+          </Routes>
+          </CartContext.Provider>
+      </BrowserRouter>
+  </>
 }
 
 export default App;
