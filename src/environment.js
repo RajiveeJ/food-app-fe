4b148c370786e590b571bea8651d5be2134ee506
RajiveeJ <99588388+RajiveeJ@users.noreply.github.com>
2023-11-18 15:09:52 +0530

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -1,5 +1,6 @@
 const env = {
-    apiurl:"https://p8000-zca0484dd-z2aa0bdd1-gtw.zb20e5e48.qovery.fr"
+    //apiurl:"https://p8000-zca0484dd-z2aa0bdd1-gtw.zb20e5e48.qovery.fr"
+    apiurl:"https://food-app-d5gr.onrender.com"
 }
 
 export default env;
